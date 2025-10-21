f855accc0874f9d497184fc688b4118bab040237
HoNgocAn <anhongoc1306@gmail.com>
2025-10-21 10:28:37 +0700

    up

--- food_delivery_customer/lib/services/notification_service.dart
+++ food_delivery_customer/lib/services/notification_service.dart
@@ -3,7 +3,6 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-
 import '../main.dart';
 import '../routes/app_routes.dart';
 
